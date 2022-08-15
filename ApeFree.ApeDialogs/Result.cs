f58bb8d73796cba5a063bb52be0ec4d98e4b4f7c
Guijie Lee <landriesnidis@yeah.net>
2022-08-15 23:56:43 +0800

    Dialog的返回类型

---
+++ ApeFree.ApeDialogs/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApeFree.ApeDialogs
+{
+    /// <summary>
+    /// Dialog返回结果
+    /// </summary>
+    public class Result: Result<object>
+    { 
+
+    }
+
+    /// <summary>
+    /// Dialog返回结果
+    /// </summary>
+    public class Result<T>
+    {
+        public bool IsCancel { get; }
+
+        public T Data { get; set; }
+    }
+}
