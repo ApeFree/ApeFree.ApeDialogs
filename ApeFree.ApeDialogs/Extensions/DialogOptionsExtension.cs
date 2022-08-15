71d76e6c8251e6ed34b199fdb75efec403575428
Guijie Lee <landriesnidis@yeah.net>
2022-08-15 23:57:47 +0800

    增加用于获取所有文本非空选项的扩展方法

---
+++ ApeFree.ApeDialogs/Extensions/DialogOptionsExtension.cs
@@ -0,0 +1,22 @@
+﻿using ApeFree.ApeDialogs.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApeFree.ApeDialogs.Settings
+{
+    public static class DialogOptionsExtension
+    {
+        /// <summary>
+        /// 获取有效选项（所有文本非空的选项）
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static IEnumerable<DialogOption> GetValidItems(this IEnumerable<DialogOption> options)
+        {
+            return options.Where(o=>!string.IsNullOrWhiteSpace(o.Text));
+
+        }
+    }
+}
