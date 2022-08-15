3ae66f5e49ad2ac39f80d75e690477a47c6c3f1e
Guijie Lee <landriesnidis@yeah.net>
2022-08-15 23:58:27 +0800

    正式开源跨平台Dialog标准库

---
+++ ApeFree.ApeDialogs/DialogProvider.cs
@@ -0,0 +1,40 @@
+﻿using ApeFree.ApeDialogs.Settings;
+
+namespace ApeFree.ApeDialogs
+{
+    /// <summary>
+    /// Dialog提供器接口
+    /// </summary>
+    public abstract partial class DialogProvider<TDialog, TView, TContext> where TDialog : BaseDialog<TView,TContext>
+    {
+        public abstract TDialog CreateDialog(TContext context);
+    }
+
+    public abstract partial class DialogProvider<TDialog, TView, TContext>
+    {
+        protected abstract TView GenerateMessageView(MessageDialogSettings setings, TContext context);
+
+        protected abstract TView GeneratePromptView(PromptDialogSettings setings, TContext context);
+
+        protected abstract TView GenerateInputView(InputDialogSettings setings, TContext context);
+
+        protected abstract TView GeneratePasswordView(PasswordDialogSettings setings, TContext context);
+
+        protected abstract TView GenerateSelectionView(SelectionDialogSettings setings, TContext context);
+
+        protected abstract TView GenerateMultipleSelectionView(MultipleSelectionDialogSettings setings, TContext context);
+
+        protected abstract TView GenerateDataTimeView(DateTimeDialogSettings setings, TContext context);
+    }
+
+    public abstract partial class DialogProvider<TDialog, TView, TContext>
+    {
+        //public Result ShowMessageDialog(MessageDialogSettings setings, TContext context)
+        //{
+        //    var dialog = CreateDialog(context);
+        //    dialog.ContentView = GenerateMessageView(setings, context);
+
+        //    return dialog.Result;
+        //}
+    }
+}
---
+++ ApeFree.ApeDialogs/IDialog.cs
@@ -0,0 +1,46 @@
+﻿using ApeFree.ApeDialogs.Settings;
+using System;
+using System.Collections.Generic;
+
+namespace ApeFree.ApeDialogs
+{
+
+    /// <summary>
+    /// 对话框标准接口
+    /// </summary>
+    public interface IDialog
+    {
+        void Show();
+        void Dismiss();
+        void Finish();
+    }
+
+    public abstract class BaseDialog<TView, TContext, TResult> : BaseDialog<TView, TContext>
+    {
+        public Result<TResult> Result { get; }
+    }
+
+    public abstract class BaseDialog<TView, TContext> : IDialog
+    {
+        public delegate void DialogEventHandler(IDialog dialog, EventArgs e);
+
+        public event DialogEventHandler Showing;
+        public event DialogEventHandler Shown;
+        public event DialogEventHandler Dismissing;
+        public event DialogEventHandler Dismissed;
+
+        public TContext Context { get; }
+
+        public TView ContentView { get; set; }
+
+        /// <summary>
+        /// 设置选项
+        /// </summary>
+        /// <param name="options"></param>
+        protected abstract void SetOptions(IEnumerable<DialogOption> options);
+
+        public abstract void Show();
+        public abstract void Dismiss();
+        public abstract void Finish();
+    }
+}
