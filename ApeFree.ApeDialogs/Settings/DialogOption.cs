6eee57d2c9bc2febd9c6d5b981abc92aaf21ff88
Guijie Lee <landriesnidis@yeah.net>
2023-03-05 00:47:50 +0800

    新增选项被选中时的回调过程，新增OptionSelected事件

--- ApeFree.ApeDialogs/Settings/DialogOption.cs
+++ ApeFree.ApeDialogs/Settings/DialogOption.cs
@@ -1,4 +1,7 @@
-﻿namespace ApeFree.ApeDialogs.Settings
+﻿using ApeFree.ApeDialogs.Core;
+using System;
+
+namespace ApeFree.ApeDialogs.Settings
 {
     /// <summary>
     /// Dialog选项信息
@@ -20,11 +23,39 @@
         /// </summary>
         public DialogOptionType OptionType { get; set; }
 
-        public DialogOption(string text = null, DialogOptionType optionType = DialogOptionType.Neutral, bool enabled = true)
+        /// <summary>
+        /// 选项被选中时的回调过程 
+        /// </summary>
+        public Action<object, OptionSelectedEventArgs> OptionSelectedCallback { get; set; }
+
+        public DialogOption(string text = null, DialogOptionType optionType = DialogOptionType.Neutral, bool enabled = true, Action<object, OptionSelectedEventArgs> callback = null)
         {
             Text = text;
             Enabled = enabled;
             OptionType = optionType;
+            OptionSelectedCallback = callback;
         }
+    }
+
+    /// <summary>
+    /// 选择选项事件参数
+    /// </summary>
+    public class OptionSelectedEventArgs : EventArgs
+    {
+        public OptionSelectedEventArgs(IDialog dialog, DialogOption option)
+        {
+            Dialog = dialog;
+            Option = option;
+        }
+
+        /// <summary>
+        /// 当前正在操作的Dialog
+        /// </summary>
+        public IDialog Dialog { get; set; }
+
+        /// <summary>
+        /// 选中的选项对应的选项信息
+        /// </summary>
+        public DialogOption Option { get; set; }
     }
 }
